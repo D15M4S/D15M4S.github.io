033af92ddade7ac495b3be9b0e96f849ec95b6a5
D15M4S <dismas5184@gmail.com>
2024-06-27 21:18:19 +0900

    fix: update add new post for testing

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719490181',resources: [
+    cacheName: 'chirpy-1719490698',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
