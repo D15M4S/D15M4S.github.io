e0109ffcc85817ec96801ce948365e240ec84880
D15M4S <dismas5184@gmail.com>
2024-06-28 17:31:56 +0900

    fix: update add new post for testing

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719502668',resources: [
+    cacheName: 'chirpy-1719550469',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
