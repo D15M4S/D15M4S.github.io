28657d4ea8bb05f9bd93524e5e53a20126e12dc9
D15M4S <dismas5184@gmail.com>
2024-08-16 23:09:44 +0900

    feat: update post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1723720312',resources: [
+    cacheName: 'chirpy-1723817181',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
